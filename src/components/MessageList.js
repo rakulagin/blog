debb60b70e07f4798279f915a05820e44076673c
rakulagin <koulagin@gmail.com>
2022-12-06 20:24:37 +0400

    поправил рендер из бд

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,36 +1,49 @@
-import React, {  useState } from 'react'
-import { readMessages } from '../firebase/firestore'
+import React, {useState} from 'react'
+import {readMessages} from '../firebase/firestore'
 import './form.css'
 
 const MessageList = () => {
-    const [messages, setMessages] = useState([{
-        header: '',
-        text: '',
-        date: '',
-        imgURI: ''
-    }])
+    // const [messages, setMessages] = useState([{
+    //     header: '',
+    //     text: '',
+    //     date: '',
+    //     imgURI: ''
+    // }])
+
+    const [messages, setMessages] = useState([])
 
     const loadMessage = async () => {
         try {
             const data = await readMessages() // вызов функции считывания из облака
             setMessages(data)
+            console.log(messages)
         } catch (error) {
             console.log(error);
         }
     }
+
+
+
     return (
         <>
             <div className='messages'>
-            <button onClick={loadMessage}>LOAD</button>
-            {messages.length > 1 &&
-                messages.map(i =>(
-                    <>
-                    <h3>{i.header}</h3>
-                    <div>{i.text}</div>
-                    <div>{i.date}</div>
-                    </>
-                ))
-            }
+                <button onClick={loadMessage}>LOAD</button>
+                {/*{messages.length > 1 && messages.map((i, index) => (*/}
+                {/*    <div key={index}>*/}
+                {/*        <h3>{i.header}</h3>*/}
+                {/*        <div>{i.text}</div>*/}
+                {/*        <div>{i.date}</div>*/}
+                {/*    </div>*/}
+                {/*))*/}
+                {/*}*/}
+
+                {messages && messages.map((i, index) => (
+                    <div key={index}>
+                        <h3>{i.header}</h3>
+                        <div>{i.text}</div>
+                        {/*<div>{i.date}</div>*/}
+                    </div>
+                ))}
             </div>
         </>
     )
