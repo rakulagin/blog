ffcaefcc67a2b6c97957d231f62940a5afce8246
rakulagin <koulagin@gmail.com>
2022-12-07 20:59:49 +0400

    начинаю верстку с ноля

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import Form from "./components/Form";
-import MessageList from "./components/MessageList";
-import Auth from './components/Auth/Auth'
-import Registration from "./components/Auth/Registration";
+
 
 function App() {
   return (
     <div className="App">
-      <header>
-          <Registration/>
-          {/*<Auth/>*/}
-          {/*<Form />*/}
-          {/*<MessageList />*/}
-      </header>
+
     </div>
   );
 }
